40145ab5fa6c0047b7c5dc9e1a9701de63167910
HeathensStudios <96132424+HeathensStudios@users.noreply.github.com>
2021-12-14 10:41:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>DoIHaveToo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="potato" />
         <p className="description">
-          How do i exist, can i leave, DOES THIS EVEN WORK?</code>
+          How do i exist, can i leave, DOES THIS EVEN WORK</code>
         </p>
       </main>
 
